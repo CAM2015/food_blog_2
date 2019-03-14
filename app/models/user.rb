c7705c50a0326002cc7406ab3c8aaaa8c250f346
Camelia <cameliafarcas.cf@gmail.com>
2019-03-14 11:38:22 +0000

    more user test and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
+    before_save {self.email = email.downcase}
+    validates :username, presence: true, length: {maximum: 40}
+  
+    # https://rubular.com... to test a regular expresion with a string to see if it matches /
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: {maximum:250},
+                      format: {with: VALID_EMAIL_REGEX },
+                      uniqueness: {case_sensitive: false}
+                      
     
 end
---
+++ test/models/user_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+    
+    def setup
+      @user = User.new(username: "Camelia", email: "camelia@gmail.com")
+    end
+    
+    test "should be valid" do
+      assert @user.valid?
+    end
+    
+    test "name should be present" do
+      @user.username = " "
+      assert_not @user.valid?
+    end
+    
+    test "name should be less than 40 characters" do
+      @user.username = "a" * 41
+      assert_not @user.valid?
+    end
+    
+    test "email should be present" do
+      @user.email = " "
+      assert_not @user.valid?
+    end
+    
+    test "email should be less than 251 characters" do
+      @user.email = "a" * 245 + '@gmail.com'
+      assert_not @user.valid?
+    end
+    
+    test "email should accept correct format" do
+      valid_emails = %w[user@example.com CAMELIA@gmail.com C.name@yahoo.com]
+      valid_emails.each do |valids|
+        @user.email = valids
+        assert @user.valid?, '#{valids.inspect} should be valid'
+      end
+    end
+    
+    test "should reject invalid addresses" do
+      invalid_emails = %w[camelia@example camelia@example,com camelia.name@gmail. cam@some+some.com]
+      invalid_emails.each do |invalids|
+        @user.email = invalids
+        assert_not @user.valid?, '#{invalids.inspect} should be invalid'
+      end
+    end
+    
+    test "email should be unique and case insensitive" do
+      duplicate_user = @user.dup
+      duplicate_user.email = @user.email.upcase
+      @user.save
+      assert_not duplicate_user.valid?
+    end
+    
+    test "email should be lowercase before hitting db" do
+      mixed_email = 'CamElIa@Example.com'
+      @user.email = mixed_email
+      @user.save
+      assert_equal mixed_email.downcase, @user.reload.email
+    end
+    
+end
